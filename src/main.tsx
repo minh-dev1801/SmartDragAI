36fead146872c75bd79157ae2dbb0d1220a325e0
minh-dev1801 <minhnv1801@gmail.com>
2025-07-22 22:19:38 +0700

    fix deploy v1

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AllRoutes from "./Routes/AllRoutes";
-import { ReactFlowProvider } from "@xyflow/react";
-import { DnDProvider } from "./provider/DnDContext";
 import { BrowserRouter } from "react-router";
 import "./styles/index.css";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ReactFlowProvider>
-        <DnDProvider>
-          <AllRoutes />
-        </DnDProvider>
-      </ReactFlowProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
